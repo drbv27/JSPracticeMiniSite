6b0baed5312f1da3a1ce35b19ed576f7f93268d8
Diego Bonilla <drbv27@gmail.com>
2021-05-27 17:42:47 -0500

    Adding the clock and his button

---
+++ js/reloj.js
@@ -0,0 +1,21 @@
+const d =document;
+
+export function digitalClock(clock,btnPlay,btnStop) {
+    d.addEventListener("click", e=>{
+        if(e.target.matches(btnPlay)){
+            setInterval(()=>{
+                let clockHour = new Date().toLocaleTimeString();
+                d.querySelector(clock).innerHTML = `<h3>${clockHour}</h3>`;
+            },1000);
+
+            e.target.disabled=true;
+        }
+
+        if(e.target.matches(btnStop)){
+
+        }
+    })
+}
+export function alarm() {
+
+}
