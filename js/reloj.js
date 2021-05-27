c64f60097951c179074a0d846e72941f5cdcb987
Diego Bonilla <drbv27@gmail.com>
2021-05-27 17:53:39 -0500

    adding stop button function

--- js/reloj.js
+++ js/reloj.js
@@ -1,9 +1,11 @@
 const d =document;
 
 export function digitalClock(clock,btnPlay,btnStop) {
+    let clockTempo;
+
     d.addEventListener("click", e=>{
         if(e.target.matches(btnPlay)){
-            setInterval(()=>{
+            clockTempo = setInterval(()=>{
                 let clockHour = new Date().toLocaleTimeString();
                 d.querySelector(clock).innerHTML = `<h3>${clockHour}</h3>`;
             },1000);
@@ -12,7 +14,9 @@ export function digitalClock(clock,btnPlay,btnStop) {
         }
 
         if(e.target.matches(btnStop)){
-
+            clearInterval(clockTempo);
+            d.querySelector(clock).innerHTML = null;
+            d.querySelector(btnPlay).disabled=false;
         }
     })
 }
