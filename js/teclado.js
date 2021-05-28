309bf767bbc523abdfbd49a6f458b7d6a77adb88
Diego Bonilla <drbv27@gmail.com>
2021-05-28 12:11:11 -0500

    Adding limits to ball(stage)

--- js/teclado.js
+++ js/teclado.js
@@ -4,9 +4,12 @@ y = 0;
 
 export function moveBall(e,ball,stage){
     const $ball = d.querySelector(ball),
-        $stage = d.querySelector(stage);
+        $stage = d.querySelector(stage),
+        limitsBall = $ball.getBoundingClientRect(),
+        limitsStage = $stage.getBoundingClientRect();
         console.log(e.keyCode);
         console.log(e.key);
+        console.log(limitsBall,limitsStage);
 
         //const move = (direction)=> {}
 
@@ -14,22 +17,22 @@ export function moveBall(e,ball,stage){
             case 37:
                 e.preventDefault();
                 //move("left")
-                x--;
+                if(limitsBall.left >limitsStage.left) x--;
                 break;
             case 38:
                 e.preventDefault();               
                 //move("up")
-                y--;
+                if(limitsBall.top > limitsStage.top) y--;
             break;
             case 39:
                 e.preventDefault();               
                // move("right")
-               x++;
+               if(limitsBall.right <limitsStage.right) x++;
             break;
             case 40:
                 e.preventDefault();               
                 //move("down")
-                y++;
+                if(limitsBall.bottom < limitsStage.bottom) y++;
             break;
             default:
                 break;
