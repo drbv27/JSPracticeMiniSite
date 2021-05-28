5d3367bb46678c97611e06a3f8d00ffa7d58e44b
Diego Bonilla <drbv27@gmail.com>
2021-05-28 11:04:13 -0500

    Adding moves to ball

--- js/teclado.js
+++ js/teclado.js
@@ -1,13 +1,46 @@
 const d = document;
+let x = 0,
+y = 0;
+
+export function moveBall(e,ball,stage){
+    const $ball = d.querySelector(ball),
+        $stage = d.querySelector(stage);
+        console.log(e.keyCode);
+        console.log(e.key);
+
+        //const move = (direction)=> {}
+
+        switch (e.keyCode) {
+            case 37:
+                //move("left")
+                x--;
+                break;
+            case 38:
+                //move("up")
+                y--;
+            break;
+            case 39:
+               // move("right")
+               x++;
+            break;
+            case 40:
+                //move("down")
+                y++;
+            break;
+            default:
+                break;
+        }
+        $ball.style.transform = `translate(${x*10}px,${y*10}px)`;
+}
 
 export function shortcuts(e){
-    console.log(e.type);
-    console.log(e.key);
-    console.log(e.keyCode);
-    console.log(e.ctrlKey);//para saber si estas presionando control
-    console.log(e.shiftKey);//para saber si estas presionando shift
-    console.log(e.altKey);//para saber si estas presionando shift
-    console.log(e);
+    // console.log(e.type);
+    // console.log(e.key);
+    // console.log(e.keyCode);
+    // console.log(e.ctrlKey);//para saber si estas presionando control
+    // console.log(e.shiftKey);//para saber si estas presionando shift
+    // console.log(e.altKey);//para saber si estas presionando shift
+    // console.log(e);
 
     if (e.key === "a" && e.altKey){
         alert("Haz lanzado una alerta con el teclado, combinando alt + a");
