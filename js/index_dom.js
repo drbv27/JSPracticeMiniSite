a79eabe3e90c1a36578ed009738df7f772d4532e
Diego Bonilla <drbv27@gmail.com>
2021-05-28 13:45:12 -0500

    First commit Countdown

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,7 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
 import {digitalClock,alarm} from "./reloj.js"
 import {moveBall,shortcuts} from "./teclado.js"
+import countdown from "./cuenta_regresiva.js"
 
 const d = document;
 
@@ -8,6 +9,7 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
     alarm("../assets/alarm-clock.mp3","#activar-alarma","#desactivar-alarma")
+    countdown("countdown","Feb 17, 2022 12:00:00","Feliz Cumpleaños...Diego!!! 🦊")
 });
 
 d.addEventListener("keydown", (e) =>{
