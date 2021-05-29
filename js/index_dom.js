2e96bb9b42fa6777fe82e00cae36f368c6a5eb00
Diego Bonilla <drbv27@gmail.com>
2021-05-29 07:06:35 -0500

    More styles create the function import export

--- js/index_dom.js
+++ js/index_dom.js
@@ -2,6 +2,7 @@ import hamburgerMenu from "./menu_hamburguesa.js";
 import {digitalClock,alarm} from "./reloj.js"
 import {moveBall,shortcuts} from "./teclado.js"
 import countdown from "./cuenta_regresiva.js"
+import scrollTopButton from "./boton_scroll.js";
 
 const d = document;
 
@@ -9,7 +10,12 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
     alarm("../assets/alarm-clock.mp3","#activar-alarma","#desactivar-alarma")
-    countdown("countdown","Feb 17, 2022 12:00:00","Feliz Cumpleaños...Diego!!! 🦊")
+    countdown(
+        "countdown",
+        "Feb 17, 2022 12:00:00",
+        "Feliz Cumpleaños...Diego!!! 🦊"
+        );
+        scrollTopButton(".scroll-top-btn");
 });
 
 d.addEventListener("keydown", (e) =>{
