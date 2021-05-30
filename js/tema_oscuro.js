9b7705ad13fb3be17363008852a4494dcae437b6
Diego Bonilla <drbv27@gmail.com>
2021-05-30 06:45:48 -0500

    Finishing the darkmode

--- js/tema_oscuro.js
+++ js/tema_oscuro.js
@@ -1,4 +1,5 @@
 const d = document;
+
 export default function darkTheme(btn,classDark){
     const $themeBtn = d.querySelector(btn),
         $selectors = d.querySelectorAll("[data-dark]");
@@ -6,6 +7,18 @@ export default function darkTheme(btn,classDark){
     console.log($selectors);
 
     let moon = "🌙",
-        sun = "☀️"
+        sun = "☀️";
 
+    d.addEventListener("click", e=>{
+        if(e.target.matches(btn)){
+            //console.log($themeBtn.textContent);
+            if($themeBtn.textContent === moon){
+                $selectors.forEach(el => el.classList.add(classDark));
+                $themeBtn.textContent = sun;
+            }else{
+                $selectors.forEach(el => el.classList.remove(classDark));
+                $themeBtn.textContent = moon;
+            }
+        }
+    })
 }
