8fc7ad6bd709e652d83420ea3e323a5e70194a3b
Diego <drbv27@gmail.com>
2021-06-02 07:48:41 -0500

    adding the isMobile object

---
+++ js/deteccion_dispositivos.js
@@ -0,0 +1,6 @@
+const d = document,
+    n = navigator,
+    ua = n.userAgent;
+export default function userDeviceInfo(id){
+    console.log(ua);
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -6,6 +6,7 @@ import scrollTopButton from "./boton_scroll.js";
 import darkTheme from "./tema_oscuro.js";
 import responsiveMedia from "./objeto_responsive.js";
 import responsiveTester from "./prueba_responsive.js";
+import userDeviceInfo from "./deteccion_dispositivos.js";
 
 const d = document;
 
@@ -32,6 +33,7 @@ d.addEventListener("DOMContentLoaded",(e)=>{
         `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.9913417051657!2d-75.56878368573402!3d6.264868027855204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e4428e695083c29%3A0x99f177323f8c67d5!2sRuta%20N!5e0!3m2!1ses!2sco!4v1622476548433!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`,
         );
         responsiveTester("responsive-tester");
+        userDeviceInfo("user-device");
 });
 
 d.addEventListener("keydown", (e) =>{
