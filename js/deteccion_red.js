63c357dce87c93e40f1cf8a78a3e98c77f84a372
Diego <drbv27@gmail.com>
2021-06-03 08:33:38 -0500

    Finish the conection detection

--- js/deteccion_red.js
+++ js/deteccion_red.js
@@ -3,6 +3,22 @@ const d = document,
     n = navigator;
 
 export default function networkStatus(){
-    w.addEventListener("online",e=>console.log(n.onLine))
-    w.addEventListener("offline",e=>console.log(n.onLine))
+    const isOnline = ()=>{
+        const $div = d.createElement("div");
+        if(n.onLine){
+            $div.textContent="Conexión Reestablecida";
+            $div.classList.add("online");
+            $div.classList.remove("offline");
+        }else{
+            $div.textContent="Conexión Perdida";
+            $div.classList.add("offline");
+            $div.classList.remove("online");
+        }
+        d.body.insertAdjacentElement("afterbegin",$div);
+        setTimeout(()=>d.body.removeChild($div), 2000);
+    };
+    w.addEventListener("online",isOnline())
+    /* w.addEventListener("online",(e)=>console.log(n.onLine)) */
+    w.addEventListener("offline",isOnline())
+    /* w.addEventListener("offline",(e)=>console.log(n.onLine)) */
 }
