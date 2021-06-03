58eebc8a325b96116770af2293d1e08e5d264b19
Diego <drbv27@gmail.com>
2021-06-03 07:48:15 -0500

    Probing the window.online by console

---
+++ js/deteccion_red.js
@@ -0,0 +1,8 @@
+const d = document,
+    w = window,
+    n = navigator;
+
+export default function networkStatus(){
+    w.addEventListener("online",e=>console.log(n.onLine))
+    w.addEventListener("offline",e=>console.log(n.onLine))
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -7,6 +7,7 @@ import darkTheme from "./tema_oscuro.js";
 import responsiveMedia from "./objeto_responsive.js";
 import responsiveTester from "./prueba_responsive.js";
 import userDeviceInfo from "./deteccion_dispositivos.js";
+import networkStatus from "./deteccion_red.js";
 
 const d = document;
 
@@ -41,4 +42,5 @@ d.addEventListener("keydown", (e) =>{
     moveBall(e,".ball",".stage")
 })
 
-darkTheme(".dark-theme-btn","dark-mode")
+darkTheme(".dark-theme-btn","dark-mode");
+networkStatus();
