3d5a2a5bf50421969f15fa64455bfca60ea1b46d
Diego <drbv27@gmail.com>
2021-06-03 16:37:18 -0500

    Implementing the geolocation

--- js/geolocalizacion.js
+++ js/geolocalizacion.js
@@ -10,11 +10,23 @@ export default function getGeolocation(id){
         };
 
         const success = position => {
+            let coords = position.coords;
             console.log(position);
+
+            $id.innerHTML =`
+            <p>Tu posición actual es:</p>
+            <ul>
+                <li>Latitud:<b>${coords.latitude}</b></li>
+                <li>Longitud:<b>${coords.longitude}</b></li>
+                <li>Precisión:<b>${coords.accuracy}</b> metros</li>
+            </ul>
+            <a href="https://www.google.com/maps/@${coords.latitude},${coords.longitude},15z" target="blank" rel="noopener">Ver en Google Maps</a>
+            `;
         }
 
         const error = err => {
-            console.log(err);
+            $id.innerHTML = `<p><mark>>${err.code}:${err.message}</mark></p`
+            console.log(`Error ${err.code}: ${err.message}`);
         }
 
         n.geolocation.getCurrentPosition(success,error,options);
