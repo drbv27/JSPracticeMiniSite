789152acbdc2613ca5375d9a59fe21cae3511d2e
Diego <drbv27@gmail.com>
2021-06-04 12:06:56 -0500

    Improving the CSS

--- js/filtro_busquedas.js
+++ js/filtro_busquedas.js
@@ -4,7 +4,10 @@ export default function searchFilters(inputs,selector){
     d.addEventListener("keyup",e=>{
         if(e.target.matches(inputs)){
             //console.log(e.key)
-            console.log(e.target.value)
+            //console.log(e.target.value);
+
+            if(e.key ==="Escape") e.target.value=""
+
             d.querySelectorAll(selector).forEach((el)=>
                 el.textContent.toLowerCase().includes(e.target.value)
                 ?el.classList.remove("filter")
