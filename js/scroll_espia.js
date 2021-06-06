d96bae09e1a1828cc5cb89cf86587f2bb21672f5
Diego <drbv27@gmail.com>
2021-06-06 18:22:41 -0500

    First test of observer

--- js/scroll_espia.js
+++ js/scroll_espia.js
@@ -4,11 +4,25 @@ export default function scrollSpy(){
     const $sections = d.querySelectorAll("section[data-scroll-spy]");
 
     const cb = (entries)=>{
-        console.log("entries",entries);
+        //console.log("entries",entries);
+
+        entries.forEach((entry)=> {
+            //console.log("entry",entry);
+            const id = entry.target.getAttribute("id");
+            //console.log(id);
+            if (entry.isIntersecting){
+                d.querySelector(`a[data-scroll-spy][href="#${id}"]`).
+                classList.add("active");
+            } else {
+                d.querySelector(`a[data-scroll-spy][href="#${id}"]`).
+                classList.remove("active");
+            }
+        })
     }
 
 
     const observer = new IntersectionObserver(cb,{});
+    //console.log("observer",observer)
 
     $sections.forEach(el=>observer.observe(el));
 }
