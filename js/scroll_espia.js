9e486302533a40027232f2859208c66d1987eead
Diego <drbv27@gmail.com>
2021-06-06 18:04:31 -0500

    first configuration of observer

---
+++ js/scroll_espia.js
@@ -0,0 +1,14 @@
+const d = document;
+
+export default function scrollSpy(){
+    const $sections = d.querySelectorAll("section[data-scroll-spy]");
+
+    const cb = (entries)=>{
+        console.log("entries",entries);
+    }
+
+
+    const observer = new IntersectionObserver(cb,{});
+
+    $sections.forEach(el=>observer.observe(el));
+}
