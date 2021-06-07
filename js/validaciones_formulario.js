353158429e406d77efce1b31db2076605290f85b
Diego <drbv27@gmail.com>
2021-06-07 18:31:35 -0500

    Finishing the form....sending by submitform webpage

--- js/validaciones_formulario.js
+++ js/validaciones_formulario.js
@@ -40,7 +40,7 @@ export default function contactFormValidations(){
         });
 
         d.addEventListener("submit", (e) =>{
-            e.preventDefault();
+            //e.preventDefault();
             alert("enviando formulario");
 
             const $loader = d.querySelector(".contact-form-loader"),
