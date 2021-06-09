339990696e59a2dcc74d6d30aa14f010b91555bc
Diego <drbv27@gmail.com>
2021-06-09 11:16:42 -0500

    Adding the ASYNC AWAIT

--- js/ajax.js
+++ js/ajax.js
@@ -7,14 +7,14 @@
     xhr.addEventListener("readystatechange", (e) => {
         if(xhr.readyState !== 4) return;
 
-        console.log(xhr);
+        //console.log(xhr);
 
         if(xhr.status >= 200 && xhr.status < 300){
-            console.log("exito");
+            //console.log("exito");
             //console.log(xhr.responseText);
             //$xhr.innerHTML = xhr.responseText;
             let json =JSON.parse(xhr.responseText);
-            console.log(json);
+            //console.log(json);
 
             json.forEach(el => {
                 const $li = document.createElement("li");
@@ -24,11 +24,11 @@
 
             $xhr.appendChild($fragment);
         }else{
-            console.log("error");
+            //console.log("error");
             let message = xhr.statusText || "Ocurrio un error";
             $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
         }
-        console.log("Este mensaje cargará de cualquier forma");
+        //console.log("Este mensaje cargará de cualquier forma");
     });
 
     xhr.open("GET","https://jsonplaceholder.typicode.com/users");
@@ -48,7 +48,7 @@
         }) */
         .then(res => res.ok ? res.json() : Promise.reject(res))
         .then(json => {
-            console.log(json);
+            //console.log(json);
             json.forEach(el => {
                 const $li = document.createElement("li");
                 $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
@@ -58,11 +58,31 @@
             $fetch.appendChild($fragment);
         })
         .catch(err => {
-            console.log(err);
+            //console.log(err);
             let message = err.statusText || "Ocurrio un error";
             $fetch.innerHTML = `Error ${err.status}: ${message}`;
-        }).finally(() => 
-            console.log("Esto se ejeecutará independientemente del resultado de la promesa Fetch"
-        )
+        }).finally(() => {
+            //console.log("Esto se ejeecutará independientemente del resultado de la promesa Fetch")
+        }
     );
+})();
+
+//Fetch async await
+
+(()=> {
+    const $fetchAsync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
+
+    async function getData(){
+        try {
+            let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+                json = await res.json();
+
+                console.log(res,json);
+        } catch (err) {
+        } finally {
+        }
+    }
+
+    getData();
 })();
