af18f9ecbad5ae09b4724de5fc0c367d33811d74
Diego <drbv27@gmail.com>
2021-06-10 07:55:35 -0500

    axios async first commit

--- js/ajax.js
+++ js/ajax.js
@@ -78,7 +78,7 @@
             let res = await fetch("https://jsonplaceholder.typicode.com/users"),
                 json = await res.json();
 
-                console.log(res,json);
+                //console.log(res,json);
 
                 //if(!res.ok) throw new Error("Ocurrio un error al solicitar los datos")
                 if(!res.ok) throw {status: res.status, statusText: res.statusText}
@@ -91,11 +91,11 @@
     
                 $fetchAsync.appendChild($fragment);
         } catch (err) {
-            console.log("Estoy en el catch",err);
+            //console.log("Estoy en el catch",err);
             let message = err.statusText || "Ocurrio un error";
             $fetchAsync.innerHTML = `Error ${err.status}: ${message}`;
         } finally {
-            console.log("Esto se ejecuta si o si")
+            //console.log("Esto se ejecuta si o si")
         }
     }
 
@@ -111,7 +111,7 @@
     axios
     .get("https://jsonplaceholder.typicode.com/users")
     .then(res => {
-        console.log(res);
+        //console.log(res);
         let json = res.data; //solo conservo el nombre pero no es un json
 
         json.forEach(el => {
@@ -123,11 +123,35 @@
         $axios.appendChild($fragment);
     })
     .catch(err => {
-        console.log(err.response);
+        //console.log(err.response);
         let message = err.response.statusText || "Ocurrio un error";
          $axios.innerHTML = `Error ${err.response.status}: ${message}`;
     })
     .finally(() => {
-        console.log("Esto se ejecuta si o si");
+        //console.log("Esto se ejecuta si o si");
     });
+})();
+
+//Axios + async await
+(()=>{
+    const $axiosAsync = document.getElementById("axios-async"),
+    $fragment = document.createDocumentFragment();
+
+    axios
+    .get("https://jsonplaceholder.typicode.com/users");
+
+    async function getData(){
+        try {
+            let res = await axios.get("https://jsonplaceholder.typicode.com/users"),
+            json = await res.data;
+
+            console.log(res,json);
+        } catch (err) {
+            
+        } finally {
+            console.log("Esto se ejecuta si o si");
+        }
+    }
+
+    getData();
 })();
