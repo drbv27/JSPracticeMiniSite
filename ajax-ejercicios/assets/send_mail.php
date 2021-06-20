ce342bc31dc0c19d73bfe6660d4ef9be3957044e
Diego <drbv27@gmail.com>
2021-06-20 06:39:34 -0500

    Final commit of send mail

--- ajax-ejercicios/assets/send_mail.php
+++ ajax-ejercicios/assets/send_mail.php
@@ -1,46 +1,45 @@
 <?php
-if(isset($_POST)){
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $comments = $_POST["comments"];
+if (isset($_POST)) {
+  error_reporting(0);
 
-    $domain = $_SERVER["HTTP_HOST"];
-    $to = "drbv27@gmail.com";
-    $subject = "Contacto desde el formulario del sitio $domain: $subject";
-    $message = "
-        <p>
-        Datos enviados desde el formulario del sitio <b>$domain</b>
-        </p>
-        <ul>
-            <li>Nombre: <b>$name</b></li>
-            <li>Email: <b>$email</b></li>
-            <li>Asunto: $subject</li>
-            <li>Comentarios:$comment</li>
-        </ul>
-    ";
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $subject = $_POST["subject"];
+  $comments = $_POST["comments"];
 
-    $headers = "MIME-Version: 1.0\r\n"."Content-Type: text/html; charset=utf-8\r\n"."From: Envío Automatico, No Responder <no-reply@domain";
-    
-    $send_mail = mail($to,$subject,$message,$headers);
+  $domain = $_SERVER["HTTP_HOST"];
+  $to = "drbv27@gmail.com";
+  $subject_mail = "Contacto desde el formulario del sitio $domain.";
+  $message = "
+    <p>
+      Datos enviados desde el formulario del sitio <b>$domain</b>
+    </p>
+    <ul>
+      <li>Nombre: <b>$name</b></li>
+      <li>Email: <b>$email</b></li>
+      <li>Asunto: $subject</li>
+      <li>Comentarios: $comments</li>
+    </ul>
+  ";
+  $headers = "MIME-Version: 1.0\r\n" . "Content-Type: text/html; charset=utf-8\r\n" . "From: Envío Automático No Responder <no-reply@$domain>";
 
-    if($send_mail) {
-        $res = [
-            "err" => false,
-            "message" => "Tus datos han sido enviados"
-        ];
-    } else {
-        $res = [
-            "err" => true,
-            "message" => "Error al enviar tus datos."
-        ];
-    }
+  $send_mail = mail($to, $subject_mail, $message, $headers);
 
-    header('Content-type:application/json');
-    echo json_encode($res);
-    exit;
+  if($send_mail) {
+    $res = [
+      "err" => false,
+      "message" => "Tus datos han sido enviados"
+    ];
+  } else {
+    $res = [
+      "err" => true,
+      "message" => "Error al enviar tus datos. Intenta nuevamente."
+    ];
+  }
+
+  //header("Access-Control-Allow-Origin: https://jonmircha.com");
+  header("Access-Control-Allow-Origin: *");
+  header( 'Content-type: application/json' );
+  echo json_encode($res);
+  exit;
 }
-
-
-
-?>
