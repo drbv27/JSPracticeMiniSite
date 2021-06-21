fddfbb3e90a420b2654d2cf5f8c260c2eec4b3fc
Diego <drbv27@gmail.com>
2021-06-21 15:01:11 -0500

    Finish the extraction of data

--- ajax-ejercicios/assets/stripe-checkout.js
+++ ajax-ejercicios/assets/stripe-checkout.js
@@ -11,7 +11,7 @@ const d = document,
     },
 }
 
-let prices,products;
+let products,prices;
 
 Promise.all([
     fetch("https://api.stripe.com/v1/products",fetchOptions),
@@ -19,7 +19,30 @@ Promise.all([
 ])
 .then(responses => Promise.all(responses.map((res)=>res.json())))
 .then(json => {
-    console.log(json)
+    //console.log(json);
+    products = json[0].data;
+    prices = json[1].data;
+    console.log(products,prices);
+
+    prices.forEach(el=>{
+        let productData = products.filter(product =>product.id === el.product)
+        console.log(productData);
+
+        $template.querySelector(".taco").setAttribute("data-price",el.id);
+        $template.querySelector("img").src = productData[0].images[0];
+        $template.querySelector("img").alt = productData[0].name;
+        $template.querySelector("figcaption").innerHTML = `
+            ${productData[0].name}
+            <br>
+            ${el.unit_amount_decimal} ${el.currency}
+        `;
+
+
+        let $clone = d.importNode($template,true);
+        $fragment.appendChild($clone);
+    });
+
+    $tacos.appendChild($fragment);
 })
 .catch(err => {
     console.log(err);
