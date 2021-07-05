05ebf68ca2f8532398318c2909b67ce6ae5fdaff
Diego <drbv27@gmail.com>
2021-07-05 07:11:11 -0500

    First UI Component:Title

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,22 +1,9 @@
 import api from "./helpers/wp_api.js";
-import {ajax} from "./helpers/ajax.js"
+import {ajax} from "./helpers/ajax.js";
+import {Title} from "./components/Title.js"
 
 export function App(){
-    document.getElementById("root").innerHTML = `<h1>Bienvenidos a mi primer SPA con Vanilla JS</h1>`
-    
-    console.log(api);
-
-    ajax({
-        url:api.POSTS,
-        cbSuccess:(posts) =>{
-            console.log(posts);
-        }
-    });
-
-    ajax({
-        url:api.CATEGORIES,
-        cbSuccess:(categories) =>{
-            console.log(categories);
-        }
-    });
+    const d = document,
+    $root = d.getElementById("root");
+    $root.appendChild(Title());
 }
