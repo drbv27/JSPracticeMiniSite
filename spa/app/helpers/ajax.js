3ed8225f3793b656ab47c6e295248fe8b1795d0f
Diego <drbv27@gmail.com>
2021-07-05 07:34:39 -0500

    Second component:Loader and some styles

--- spa/app/helpers/ajax.js
+++ spa/app/helpers/ajax.js
@@ -2,13 +2,13 @@ export function ajax(props){
     let { url,cbSuccess }=props;
 
     fetch(url)
-        .then(res => res.ok ? res.json(): Promise.reject())
+        .then(res => res.ok ? res.json() : Promise.reject(res))
         .then(json => cbSuccess(json))
         .catch(err => {
             let message = err.statusText || "Ocurrio un error al acceder a la API";
 
             document.getElementById("root").innerHTML=`
-                <div class="error>
+                <div class="error">
                 <p>Error ${err.status}: ${message}</p>
                 </div>
             `;
