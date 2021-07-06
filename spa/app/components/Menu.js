381eb419fae09dd95ecd161cce8b7584fef86653
Diego <drbv27@gmail.com>
2021-07-06 15:28:31 -0500

    Inserting the Form Component

--- spa/app/components/Menu.js
+++ spa/app/components/Menu.js
@@ -7,7 +7,6 @@ export function Menu(){
         <a href="#/search">Busqueda</a>
         <span>-</span>
         <a href="https://aprendejavascript.org" target="_blank" rel="noopener">Aprende JS</a>
-
     `;
     return $menu;
 }
--- spa/app/components/SearchForm.js
+++ spa/app/components/SearchForm.js
@@ -1,4 +1,11 @@
 export function SearchForm(){
-    const $form = document.createElement("form");
+    const $form = document.createElement("form"),
+        $input = document.createElement("input");
+
+    $form.classList.add("form-search");
+    $input.name = "search";
+    $input.type = "search";
+    $input.placeholder = "Buscar...";
+    $form.appendChild($input)
     return $form;
 }
