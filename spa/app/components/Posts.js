f6e17de16e1047afb99b37443e81c1dceda874fe
Diego <drbv27@gmail.com>
2021-07-07 15:01:40 -0500

    Responsive design only grid css

--- spa/app/components/Posts.js
+++ spa/app/components/Posts.js
@@ -1,5 +1,6 @@
 export function Posts(props){
     const $posts = document.createElement("section");
         $posts.id = "posts";
+        $posts.classList.add("grid-fluid")
         return $posts
 }
