e8c7ee1ad5cba5d8e794b58af1b83804d8dab9bb
Diego <drbv27@gmail.com>
2021-07-07 17:33:00 -0500

    Second configuration Router

--- spa/app/components/Router.js
+++ spa/app/components/Router.js
@@ -4,21 +4,33 @@ import { PostCard } from "./PostCard.js";
 
 export function Router(){
     const d = document,
-        w = window;
+        w = window,
+        $posts = d.getElementById("posts");
 
     let{hash}=location;
-
     console.log(hash);
 
-    ajax({
-        url:api.POSTS,
-        cbSuccess:(posts)=>{
-            //console.log(posts);
-            let html = "";
-            posts.forEach((post)=> html += PostCard(post));
-            d.querySelector(".loader").style.display = "none";
-            d.getElementById("posts").innerHTML=html;
-        },
-    });
+    $posts.innerHTML=null;
+
+    if(!hash || hash ==="#/"){
+        ajax({
+            url:api.POSTS,
+            cbSuccess:(posts)=>{
+                //console.log(posts);
+                let html = "";
+                posts.forEach((post)=> html += PostCard(post));
+                d.querySelector(".loader").style.display = "none";
+                $posts.innerHTML=html;
+            },
+        });
+    } else if(hash.includes('#/search')){
+        $posts.innerHTML="<h2>Sección del Buscador</h2>";
+    } else if(hash === '#/contacto'){
+        $posts.innerHTML="<h2>Sección de Contacto</h2>";
+    }else{
+        $posts.innerHTML="<h2>Aquí cargará el contenido del post previamente seleccionado</h2>";
+
+    }
+/* */
 
 }
