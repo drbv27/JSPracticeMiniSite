3de8524da64ece39babebccc36934e8513383887
Diego <drbv27@gmail.com>
2021-07-11 16:09:51 -0500

    Showing the search results

--- spa/app/components/Router.js
+++ spa/app/components/Router.js
@@ -2,6 +2,7 @@ import api from "../helpers/wp_api.js";
 import {ajax} from "../helpers/ajax.js"
 import { PostCard } from "./PostCard.js";
 import { Post } from "./Post.js";
+import { SearchCard } from "./SearchCard.js";
 
 export async function Router(){
     const d = document,
@@ -26,12 +27,27 @@ export async function Router(){
     } else if(hash.includes('#/search')){
         let query = localStorage.getItem("wpSearch");
 
-        if(!query) return false;
+        if(!query){
+            d.querySelector(".loader").style.display="none";
+            return false;
+        }
 
         await ajax({
             url:`${api.SEARCH}${query}`,
             cbSuccess:(search)=>{
                 console.log(search);
+                let html="";
+                if(search.length===0){
+                    html=`
+                    <p class="error">
+                        No existe resultados de busqueda para el termino
+                        <mark>${query}</mark>
+                    </p>
+                `;
+                }else{
+                    search.forEach((post)=> (html+=SearchCard(post)));
+                }
+                $main.innerHTML=html;
             }
         })
 
